80b0c11219cd1c6615bd4081c4c77a10c97e2504
JoelM00 <61049184+JoelM00@users.noreply.github.com>
2021-03-30 16:14:42 +0100

    Add files via upload

---
+++ src/FastFileServer.java
@@ -0,0 +1,5 @@
+public class FastFileServer {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/HttpWorker.java
@@ -0,0 +1,66 @@
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.Scanner;
+
+public class HttpWorker implements Runnable {
+    Socket s;
+    BufferedReader in;
+    PrintWriter out;
+
+    public HttpWorker(Socket s) throws IOException {
+        this.s = s;
+        this.in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        this.out = new PrintWriter(s.getOutputStream());
+    }
+
+    @Override
+    public void run() {
+        try {
+            String get = in.readLine();
+            String[] tokens = get.split(" ", 3);
+            String ficheiro = tokens[1].substring(1);
+
+            File f = new File("./ficheiros/"+ficheiro);
+            InputStream file = new FileInputStream(f);
+
+            Scanner sc = new Scanner(f);
+
+            int amountBytesToRead = (int) f.length();
+            int positionToRead = 0;
+            FileInputStream fis = new FileInputStream("./ficheiros/"+ficheiro);
+
+            //A direct ByteBuffer should be slightly faster than a 'normal' one for IO-Operations
+            ByteBuffer bytes = ByteBuffer.allocateDirect(amountBytesToRead);
+            fis.getChannel().read(bytes, positionToRead);
+
+            byte[] readBytes = bytes.array();
+
+            System.out.println(new String(readBytes));
+            //Handle Bytes
+
+            /*
+            while (sc.hasNext()) {
+                System.out.println(sc.nextLine());
+            }*/
+            /*
+            for (int i = 0; i<6; i++) {
+                System.out.println(in.readLine());
+            }*/
+
+            String httpResp = "HTTP/1.1 200 OK\n" +
+                    "Server: Apache\n" +
+                    "Content-Length: 187\n" +
+                    "Content-Type: text\n" +
+                    "\n";
+
+            out.print(httpResp);
+            out.flush();
+            while (sc.hasNext()) {
+                out.println(sc.nextLine());
+                out.flush();
+            }
+        } catch (Exception e) {}
+    }
+}
